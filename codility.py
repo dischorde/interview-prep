8d0f58e736780342ed5701cefc6fb483a1db3aa2
Katarina Rossi <katarina.n.rossi@gmail.com>
2017-04-08 20:50:06 -0700

    Add codility lesson 3 problems

--- codility.py
+++ codility.py
@@ -41,3 +41,36 @@ def cyclic_rotation(arr, rotation):
         return []
     shift = rotation % len(arr)
     return arr[-shift:] + arr[:-shift]
+
+def perm_missing_element(arr):
+    sum = 0
+    for expected in xrange(1,len(arr) + 2):
+        sum += expected
+
+    for actual in arr:
+        sum -= actual
+
+    return sum
+
+from math import ceil
+
+def frog_jumps(starting_point, destination, jump_distance):
+    exact_jumps = (destination - starting_point) / float(jump_distance)
+    return int(ceil(exact_jumps))
+
+def tape_equilibrium(arr):
+    left_sum = 0
+    right_sum = 0
+    minimum = None
+
+    for el in arr:
+        right_sum += el
+
+    for idx, el in enumerate(arr):
+        current_difference = abs(right_sum - left_sum)
+        if idx > 0 and (minimum == None or current_difference < minimum):
+            minimum = current_difference
+        left_sum += el
+        right_sum -= el
+
+    return minimum
